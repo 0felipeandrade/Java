323c582a29d415ea80cec5c9d7caa953e282c312
Felipe Andrade <felipeandradefna@gmail.com>
2026-08-27 12:09:13 -0300

    Update Conta.java

--- Lista01-ProgramaçãoModular/Ex02/Conta.java
+++ Lista01-ProgramaçãoModular/Ex02/Conta.java
@@ -1,50 +1,82 @@
-public class Conta {
 
-    private int numeroDaConta;
-    private double saldoDaConta;
+import java.util.Scanner;
+
+public class Conta{
+
+    Scanner leitor = new Scanner(System.in);
+
+
+    private String nomeDoTitular;
+    private int numConta;
+    private double saldo;
+
+    public double controleDeOperacao;
+
 
     public Conta(){
 
-        this.numeroDaConta = 00000;
-        this.saldoDaConta = 0.0;
+        this.nomeDoTitular = "NOMEUSUARIO";
+        this.numConta = 0;
+        this.saldo = 0.0;
 
     }
 
-    public Conta(int numeroDaConta, double saldoDaConta){
+    public void setConta(){
 
-        this.numeroDaConta = numeroDaConta;
-        this.saldoDaConta = saldoDaConta;
+        System.out.print("Digite seu nome completo: ");
+        this.nomeDoTitular = leitor.nextLine();
+
+        System.out.printf("\nDigite o numero da sua conta: ");
+        this.numConta = leitor.nextInt();
 
     }
 
-    public void setDepositar(double valor){
+    private void Deposito(){
 
-        this.saldoDaConta = saldoDaConta + valor;
+        System.out.print("Digite a quantidade que deseja depositar:");
+        controleDeOperacao = leitor.nextDouble();
+
+        saldo = saldo + controleDeOperacao;
+
+    }
+
+    private void Sacar(){
+
+        System.out.print("Digite a quantidade que deseja sacar:");
         
-        System.out.println("DEPOSITO REALIZADO COM SUCESSO");
-    
-    }
+        controleDeOperacao = leitor.nextDouble();
+        
 
-    public void setSacar(double valor){
+        if((saldo - controleDeOperacao) < 0){
 
-        if(saldoDaConta <= 0 || (saldoDaConta - valor) < 0){
-
-            System.out.println("SALDO INSUFICIENTE");
+            System.out.println("ERRO: SALDO INSULFICIENTE");
 
         }else{
 
-            this.saldoDaConta = saldoDaConta - valor;
-
-            System.out.println("SAQUE FEITO COM SUCESSO!!");
+            saldo = saldo - controleDeOperacao;
+            
+            System.out.println("SAQUE REALIZADO COM SUCESSO");
         }
 
-
     }
 
-    public double ConfereSaldo(){
+    public Double getConfereSaldo(){
 
-        return saldoDaConta;
-
+        return saldo;
     }
+
+    public String setDeposito(){
+
+        this.Deposito();
+        
+        return "Deposito Feito com sucesso";
+    }
+
+    public void setSacar(){
+
+        this.Sacar();
+    }
+
+
     
 }
