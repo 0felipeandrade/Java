c4b07941d3fd52404abd2e84715ab9043d05f356
Felipe Andrade <felipeandradefna@gmail.com>
2026-08-27 12:08:33 -0300

    Update Main.java

--- Lista01-ProgramaçãoModular/Ex02/Main.java
+++ Lista01-ProgramaçãoModular/Ex02/Main.java
@@ -1,29 +1,12 @@
-import java.util.Scanner;
 
-public class Main {
-    
+
+public class Main{
+
     public static void main(String[] args) {
+    
+    BancoInterface startPrograma = new BancoInterface();
 
-        Scanner leitor = new Scanner(System.in);
+    startPrograma.Menu();
 
-        int numConta;
-        String nomeDoTitular;
-
-        System.out.println("DIGITE O NUMERO DA SUA CONTA E O NOME DO TITULAR:");
-
-        System.out.print("NUMERO DA CONTA: ");
-        numConta = leitor.nextInt();
-
-        leitor.nextLine();
-
-        System.out.printf("\nNOME DO TITULAR: ");
-        nomeDoTitular = leitor.nextLine();
-
-
-        ContaInterface.menu(numConta,nomeDoTitular);
-
-        leitor.close();
-
-        
     }
 }
