ac445df262fc7420132892bc9809ff13a6635037
Felipe Andrade <felipeandradefna@gmail.com>
2026-08-31 22:45:16 -0300

    Update BancoInterface.java

--- Lista01-ProgramaçãoModular/Ex03/BancoInterface.java
+++ Lista01-ProgramaçãoModular/Ex03/BancoInterface.java
@@ -3,63 +3,114 @@ import java.util.Scanner;
 
 public class BancoInterface{
 
-    Banco acesso;
+    Banco acessoAoBanco;
+    ContaInterface manipulaConta;
+
+    Conta criaConta;
 
     public BancoInterface(){
 
-        acesso = new Banco();
+        acessoAoBanco = new Banco();
+        manipulaConta = new ContaInterface();
 
     }
 
-    public void Menu(){
+    public void menuBancoPrincipal(){  
 
-        Scanner leitor = new Scanner(System.in);
+        boolean whileRodaMenuBanco = true;
         int flag;
-        int SairPrograma = 1;
-        ContaInterface minhaConta;
-        Conta temporario;
+        String nome;
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Seja bem-vindo ao Banco POO Master.");
 
+        while(whileRodaMenuBanco){
 
-        while(SairPrograma == 1){
-
-        System.out.println("DESEJA CRIAR UMA CONTA?");
-        System.out.printf("1-SIM\n2-NAO\n3-SAIRDOPROGRAMA");
-
-        flag = leitor.nextInt();
+            System.out.printf("Qual das seguintes opcoes melhor te atende:\n1-Abrir Nova Conta\n2-Selecionar alguma conta \n3- Sair do Programa\n\n-----------------\n");
+            flag = leitor.nextInt();
 
             switch (flag) {
                 case 1:
+                    System.out.println("Vamos criar sua conta...");
+                    System.out.printf("Digite seu nome completo:\n");
 
-                    minhaConta = new ContaInterface();
-                    temporario = minhaConta.criaConta();
-                    minhaConta.AcessaConta();
-                    acesso.CadastroDeConta(temporario);
+                    leitor.nextLine();
+
+                    nome = leitor.nextLine();
+
+
+                    System.out.printf("Digite um numero para sua conta:");
+
+                    flag = leitor.nextInt();
+                    
+                    criaConta = new Conta(flag,nome);
+
+                    acessoAoBanco.cadastrarContas(criaConta);
+
                     break;
-
+            
                 case 2:
 
-                    int contador = 0;
-                    while(contador < acesso.qtdContas){
+                    int guardaPosicao = -1;
+                    Conta temp;
+                    System.out.println("Digite qual o numero da conta que deseja acessar: ");
+
+
+                    flag = leitor.nextInt();
+
+                    for(int i = 0; i < acessoAoBanco.getRetornaInteiroQuantidadeContas();i++){
+
+                        temp = acessoAoBanco.acessoUsuario()[i];
+
+                        if(temp.getNumeroConta() == flag){
+                            
+                            System.out.println("A conta com o numero" + temp.getNumeroConta() + " pertence a " + temp.getnomeConta());
+                            guardaPosicao = i;
+                            break;
+
+                        }else if(i == (acessoAoBanco.getRetornaInteiroQuantidadeContas() - 1)){
+                            
+                            System.out.println("Conta nao encontrada chefe...");
+                            break;
+
+                        }
+
+                    }
+
+                    if(guardaPosicao != -1){
+
+                        System.out.println("Deseja acessar a conta?" + acessoAoBanco.acessoUsuario()[guardaPosicao].getnomeConta());
                         
-                        System.out.println(acesso.AcessaContaUsuario()[contador]);
-                        
-                        contador++;
-                    }       break;
-                case 3:
-                    
-                    System.out.println("Obrigado por utilizar nossos servicos");
-                    SairPrograma = 0;
+                        System.out.printf("1- Sim\n 2-Nao");
+
+                        flag = leitor.nextInt();
+
+                        if(flag == 1){
+
+                            manipulaConta.AcessaContaInterface(acessoAoBanco.acessoUsuario()[guardaPosicao]);
+                            manipulaConta.menu();
+
+                        }else{
+
+                            System.out.println("Nao vamos acessa-la");
+                        }
+
+                    }
+
+
                     break;
-                    
-                default:
+                
+                case 3:
+
+                    System.out.println("Saindo do programa...");
+                    whileRodaMenuBanco = false;
+                                            
                     break;
             }
 
+
         }
-        
-
-
 
     }
+
 }
