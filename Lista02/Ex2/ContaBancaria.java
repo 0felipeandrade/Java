3d2ea927630768d49895074697e47c31b3a693d0
Felipe Andrade <felipeandradefna@gmail.com>
2026-09-21 17:30:19 -0300

    Refactor ContaBancaria to abstract class and update methods

--- Lista02/Ex2/ContaBancaria.java
+++ Lista02/Ex2/ContaBancaria.java
@@ -5,6 +5,13 @@ public abstract class ContaBancaria {
     protected double saldo;
 
     public ContaBancaria(String cliente,String numConta,double saldo){
+public abstract class ContaBancaria {
+    
+    protected String cliente;
+    protected String numConta;
+    protected double saldo;
+
+    public ContaBancaria(String cliente,String numConta,double saldo){
 
         this.cliente = cliente;
         this.numConta = numConta;
@@ -26,9 +33,49 @@ public abstract class ContaBancaria {
         }
     }
 
-    protected void depositar(double valor){
+    public double depositar(double valor){
 
         saldo = saldo + valor;
+
+        return saldo;
+
+    }
+
+    public String getNumConta(){
+
+        return numConta;
+    }
+
+    public double getSaldo(){
+
+        return saldo;
+    }
+}
+        this.cliente = cliente;
+        this.numConta = numConta;
+        this.saldo = saldo;
+        
+    }
+
+    protected boolean sacar(double valor){
+
+        if((saldo - valor) < 0){
+
+            return false;
+
+        }else{
+
+            saldo = saldo - valor;
+
+            return true;
+        }
+    }
+
+    public double depositar(double valor){
+
+        saldo = saldo + valor;
+
+        return saldo;
 
     }
 
